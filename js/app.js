c7077f9241a0d6a124e81817f3d32da7be00abef
EstanisEVL <estanislaovl@gmail.com>
2023-11-15 11:49:37 -0300

    feat: Add display function and basic gui

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const INITIAL_VALUE = 0;
+let displayValue = INITIAL_VALUE;
 let num1 = 100;
 let num2 = 3;
 let operator = "/";
@@ -45,3 +47,39 @@ const operate = (operator, num1, num2) => {
       return divide(num1, num2);
   }
 }
+
+const display = (e) => {
+  // RECIBE UN VALOR DE CADA BOTÓN, LO VA GUARDANDO EN EL DISPLAYVALUE Y LO DEVUELVE
+  let input = e.target.dataset.value;
+
+  if(Number(displayValue) === INITIAL_VALUE) {
+    displayValue = input;
+    displayDiv.textContent = `${displayValue}`;
+  } else {
+    displayValue += String(input);
+    displayDiv.textContent = `${displayValue}`;
+  }
+}
+
+const clearDisplay = () => {
+  displayValue = INITIAL_VALUE;
+
+  displayDiv.textContent = `${displayValue}`;
+}
+
+const displayDiv = document.querySelector(".display-container");
+displayDiv.textContent = `${displayValue}`;
+
+const digitBtn = document.querySelectorAll(".btn-digit");
+digitBtn.forEach(btn => {
+  btn.addEventListener("click", display);
+})
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearDisplay);
+
+/*
+Create the functions that populate the display when you click the number buttons.
+
+You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+*/
